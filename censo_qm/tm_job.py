0c7dd6e8e37c17261a1dd0bb2030331166f22b4c
fabothch <49951809+fabothch@users.noreply.github.com>
2022-05-04 20:32:46 +0200

    indentation

--- censo_qm/tm_job.py
+++ censo_qm/tm_job.py
@@ -955,7 +955,7 @@ class TmJob(QmJob):
                 callargs = callargs + ["--uhf", str(self.job["unpaired"])]
             # update charge:
             if int(self.job["charge"]) != 0:
-            callargs = callargs + ["--chrg", str(self.job["charge"])]
+                callargs = callargs + ["--chrg", str(self.job["charge"])]
 
             with open(
                 os.path.join(self.job["workdir"], "opt.inp"), "w", newline=None
