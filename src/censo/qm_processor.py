003ab9f5343c6509bb43a8d40858f896adaaa3b3
Polt <seidler118@gmail.com>
2024-02-03 11:05:38 +0100

    xtb_sp fix metadata

--- src/censo/qm_processor.py
+++ src/censo/qm_processor.py
@@ -353,7 +353,6 @@ class QmProc:
                 # TODO - important - what to do if calculation not converged?
 
         # FIXME - right now the case meta["success"] = None might appear if "TOTAL ENERGY" is not found in outputfile
-        job.meta["xtb_sp"].update(meta)
         return result
 
     def _xtb_gsolv(self, job: ParallelJob, jobdir: str) -> tuple[dict[str, float | None], dict[str, any]]:
