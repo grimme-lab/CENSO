55b4ceae4cfb24fb83e8ba58d970609840d707e6
Polt <seidler118@gmail.com>
2024-05-29 12:09:01 +0200

    commit hash fix

--- src/censo/__version__.py
+++ src/censo/__version__.py
@@ -1,6 +1,8 @@
 import git
+import os
 
-repo = git.Repo(search_parent_directories=True)
+repo = git.Repo(os.path.dirname(os.path.realpath(__file__)),
+                search_parent_directories=True)
 sha = repo.head.object.hexsha
 sha_short = repo.git.rev_parse(sha, short=7)
 __version__ = f"2.0.0 {sha_short}"
