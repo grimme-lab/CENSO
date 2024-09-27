f70dc550ddd30b6d10b670b239b31618b198d9f9
Polt <seidler118@gmail.com>
2024-09-27 11:08:56 +0200

    tm proc basic setup

---
+++ src/censo/tm_processor.py
@@ -0,0 +1,217 @@
+"""
+Contains TmProc class for calculating TURBOMOLE related properties of conformers.
+"""
+import subprocess
+import os
+
+from .qm_processor import QmProc
+from .logging import setup_logger
+from .parallel import ParallelJob
+from .params import ENVIRON
+from .datastructure import GeometryData
+
+logger = setup_logger(__name__)
+
+
+class TmProc(QmProc):
+    """
+    Performs calculations using TURBOMOLE.
+    """
+
+    __gridsettings = {
+        "low": ["-grid", "m3", "-scfconv", "6"],
+        "low+": ["-grid", "m4", "-scfconv", "6"],
+        "high": ["-grid", "m4", "-scfconv", "7"],
+        "high+": ["-grid", "m5", "-scfconv", "7"],
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # expand jobtypes with special turbomole jobtypes
+        self._jobtypes = {
+            **self._jobtypes,
+            **{
+                "sp": self._sp,
+                "gsolv": self._gsolv,
+                "xtb_opt": self._xtb_opt,
+                "opt": self._opt,
+                "nmr": self._nmr,
+                "rot": self._rot,
+            },
+        }
+
+        # Stores setting wether to copy MO-files for faster SCFs
+        self.copy_mo: bool = False
+        """self._jobtypes = {
+            **self._jobtypes, **{
+                "opt": self._opt,
+                "uvvis": self._uvvis,
+            }
+        }"""
+
+    def __prep(self,
+               job: ParallelJob,
+               jobtype: str,
+               jobdir: str,
+               no_solv: bool = False,
+               coordfile: str = None) -> list:
+        """
+        Prepares TURBOMOLE input files using cefine for a specified jobtype.
+        """
+        func = job.prepinfo[jobtype]["func_name"]
+        basis = job.prepinfo[jobtype]["basis"]
+        disp = job.prepinfo[jobtype]["disp"]
+
+        # Set up basic cefine call
+        call = [
+            self._paths["cefinepath"], "-func", func, "-bas", basis, "-sym",
+            "c1", "-noopt"
+        ]
+
+        # Configure grid
+        call.extend(self.__gridsettings[job.prepinfo["grid"]])
+
+        # r2scan-3c should use m4 grid
+        if func == "r2scan-3c":
+            call[call.index("m3")] = "m4"
+
+        # Configure GCP settings
+        if job.prepinfo["gcp"]:
+            call.extend()
+
+        # Add dispersion
+        # dispersion correction information
+        # FIXME - temporary solution (not very nice)
+        mapping = {
+            "d3bj": "-d3",
+            "d3(0)": "-zero",
+            "d4": "-d4",
+            "novdw": "-novdw",
+            "included": "-novdw",
+        }
+
+        if disp not in ["composite", "nl"]:
+            call.extend(mapping[disp])
+        elif disp == "nl":
+            # TODO - write $donl in control later
+            pass
+
+        # Add charge and unpaired info
+        if job.prepinfo["unpaired"] > 0:
+            call.extend(["-uhf", f"{job.prepinfo['unpaired']}"])
+        if job.prepinfo["charge"] != 0:
+            call.extend(["-chrg", f"{job.prepinfo['charge']}"])
+
+        # Call cefine
+        cefine_output = subprocess.check_output(
+            call,
+            shell=False,
+            text=True,
+            stdin=None,
+            stderr=subprocess.PIPE,
+            cwd=jobdir,
+            env=ENVIRON,
+        ).decode("utf-8").splitlines()
+
+        # Check output for errors
+        for line in cefine_output:
+            """
+            if "define ended abnormally" in line:
+                self.job["success"] = False
+                broken = True
+            elif "define_huge" in line:
+                print(f"{'ERROR:':{WARNLEN}}define_huge: not found!")
+                self.job["success"] = False
+                broken = True
+            elif "Could not find the beginning of the MO-eigenvalue data" in line:
+                self.job["success"] = False
+                broken = True
+            """
+            pass
+
+        # TODO - do further manipulations of input files
+        self.__prep_main()
+        self.__prep_solv()
+        self.__prep_nmr()
+
+    def __prep_main(self):
+        # TODO - Special treatment for KT1/KT2
+        with open(os.path.join(self.job["workdir"], "control"),
+                  "r",
+                  newline=None) as inp:
+            tmp = inp.readlines()
+        with open(os.path.join(self.job["workdir"], "control"),
+                  "w",
+                  newline=None) as out:
+            for line in tmp:
+                if "functional" in line:
+                    out.write("   functional xcfun set-gga  \n")
+                    if (dfa_settings.functionals.get(
+                            self.job["func"]).get("tm") == "kt2"):
+                        out.write("   functional xcfun kt2 1.0  \n")
+                    elif (dfa_settings.functionals.get(
+                            self.job["func"]).get("tm") == "kt1"):
+                        out.write("   functional xcfun kt1 1.0  \n")
+                else:
+                    out.write(line + "\n")
+
+    def __prep_solv(self):
+        pass
+
+    def __prep_nmr(self, prepinfo: dict[str, any], jobtype: str, jobdir: str):
+        # Set NMR parameters
+        if "nmr" in jobtype:
+            # Determine the settings that need to be put into the input file for the NMR calculation
+            active_elements_map = {
+                '"H"': prepinfo[jobtype]["h_active"],
+                '"C"': prepinfo[jobtype]["c_active"],
+                '"F"': prepinfo[jobtype]["f_active"],
+                '"Si"': prepinfo[jobtype]["si_active"],
+                '"P"': prepinfo[jobtype]["p_active"],
+            }
+
+            todo = [
+                element for element in active_elements_map.keys()
+                if active_elements_map[element]
+            ]
+
+            with open(os.path.join(jobdir, "control"), "r") as inp:
+                lines = inp.readlines()
+
+            rpacor_line_index = next(
+                lines.index(l) for l in lines if "rpacor" in l)
+            rpacor = float(lines[rpacor_line_index].split()[-1])
+            rpacor = rpacor if rpacor > 10000 else 10000
+            lines[rpacor_line_index] = f"$rpacor {rpacor}\n"
+
+            lines[-1:-1] = ["$ncoupling\n", " simple\n", " thr=0.0\n"]
+
+            if not all(element in todo for element in active_elements_map):
+                lines[-1:-1] = [
+                    "$nucsel " + " ".join(todo) + "\n",
+                    "$nucsel2 " + " ".join(todo) + "\n"
+                ]
+
+            lines.insert(-1, "$rpaconv 8\n")
+
+            with open(os.path.join(jobdir, "control"), "w") as inp:
+                inp.writelines(lines)
+
+    def _sp(self):
+        pass
+
+    def _gsolv(self):
+        pass
+
+    def _xtb_opt(self):
+        pass
+
+    def _opt(self):
+        pass
+
+    def _nmr(self):
+        pass
+
+    def _rot(self):
+        pass
