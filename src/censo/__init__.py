22a77b64967196337aa06fb08e2d098c2284e3d6
lmseidler <seidler118@gmail.com>
2024-10-28 15:26:29 +0100

    small fix

--- src/censo/__init__.py
+++ src/censo/__init__.py
@@ -1,11 +1,21 @@
 from .configuration import configure
-from .params import DESCR
+from .params import Params
 from .__version__ import __version__
 
-print(DESCR)
+print(Params.DESCR)
 configure()
 
 from .cli import interface, cml_parser
-from . import (configuration, ensembledata, datastructure, orca_processor,
-               parallel, part, procfact, qm_processor, utilities, ensembleopt,
-               properties)
+from . import (
+    configuration,
+    ensembledata,
+    datastructure,
+    orca_processor,
+    parallel,
+    part,
+    procfact,
+    qm_processor,
+    utilities,
+    ensembleopt,
+    properties,
+)
