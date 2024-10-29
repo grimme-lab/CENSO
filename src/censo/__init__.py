e7a8b6a4520e8e7a31b4c6d135d7d14b93e3b251
Polt <seidler118@gmail.com>
2024-10-29 15:02:22 +0100

    Revert "small fix"
    This reverts commit 22a77b64967196337aa06fb08e2d098c2284e3d6.

--- src/censo/__init__.py
+++ src/censo/__init__.py
@@ -1,21 +1,11 @@
 from .configuration import configure
-from .params import Params
+from .params import DESCR
 from .__version__ import __version__
 
-print(Params.DESCR)
+print(DESCR)
 configure()
 
 from .cli import interface, cml_parser
-from . import (
-    configuration,
-    ensembledata,
-    datastructure,
-    orca_processor,
-    parallel,
-    part,
-    procfact,
-    qm_processor,
-    utilities,
-    ensembleopt,
-    properties,
-)
+from . import (configuration, ensembledata, datastructure, orca_processor,
+               parallel, part, procfact, qm_processor, utilities, ensembleopt,
+               properties)
