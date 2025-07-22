eb23c13ec598eb1ac0e86730d7cabf65b7fe0d8b
lmseidler <seidler118@gmail.com>
2025-07-22 11:15:02 +0200

    avoid duplicate logger file handlers + no more global logger list (using registry with name match instead)

--- src/censo/logging.py
+++ src/censo/logging.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 __loglevel = logging.INFO
 
-__loggers = []
-
 # _loglevel = logging.DEBUG
 
 
@@ -36,26 +34,33 @@ def setup_logger(name: str) -> logging.Logger:
         # Add the StreamHandler to the logger
         logger.addHandler(stream_handler)
 
-    if logger not in __loggers:
-        __loggers.append(logger)
-
     return logger
 
 
 def set_filehandler(path: str | Path):
-    """Set filehandler for all loggers."""
-    handler = logging.FileHandler(path)
+    """Set filehandler for all censo loggers, avoiding duplicates."""
+    filehandler_path = str(path)
     formatter = logging.Formatter(
         "{asctime:24s}-{name:^24s}-{levelname:^10s}- {message}", style="{"
     )
-    handler.setFormatter(formatter)
-    for logger in __loggers:
-        logger.addHandler(handler)
+    # Loop over all loggers registered in logging
+    for logger_name, logger in logging.Logger.manager.loggerDict.items():
+        if isinstance(logger, logging.Logger) and logger_name.startswith("censo"):
+            # Check for pre-existing FileHandlers with same path
+            filehandler_exists = any(
+                isinstance(h, logging.FileHandler)
+                and getattr(h, "baseFilename", None) == filehandler_path
+                for h in logger.handlers
+            )
+            if not filehandler_exists:
+                handler = logging.FileHandler(path)
+                handler.setFormatter(formatter)
+                logger.addHandler(handler)
 
 
 def set_loglevel(loglevel: str) -> None:
     """
-    Set the log level for the logger.
+    Set the log level for all censo loggers.
 
     Args:
         loglevel (str): The log level to set.
@@ -65,5 +70,6 @@ def set_loglevel(loglevel: str) -> None:
     """
     global __loglevel
     __loglevel = getattr(logging, loglevel)
-    for logger in __loggers:
-        logger.setLevel(__loglevel)
+    for logger_name, logger in logging.Logger.manager.loggerDict.items():
+        if isinstance(logger, logging.Logger) and logger_name.startswith("censo"):
+            logger.setLevel(__loglevel)
