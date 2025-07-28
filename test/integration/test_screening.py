c0c713111a1c0270abef32a370e4a41115cac739
lmseidler <seidler118@gmail.com>
2025-07-28 13:00:44 +0200

    updated screening integration test

--- test/integration/test_screening.py
+++ test/integration/test_screening.py
@@ -5,17 +5,20 @@ from censo.config.parts_config import PartsConfig
 from censo.config.parallel_config import ParallelConfig
 from censo.params import SOLV_MODS, OrcaSolvMod, QmProg, TmSolvMod
 
-# Only test orca and tm (not xtb)
-PROGS = [QmProg.ORCA.value, QmProg.TM.value]
-
 
 # Parameterized test cases for screening
 @pytest.mark.optional
 @pytest.mark.parametrize(
-    "prog, solvation_model, gas_phase",
+    "prog, solvation_model, evaluate_rrho",
     [
         pytest.param(QmProg.ORCA, None, True, marks=pytest.mark.requires_orca),
         pytest.param(QmProg.TM, None, True, marks=pytest.mark.requires_turbomole),
+        pytest.param(
+            QmProg.ORCA, OrcaSolvMod.SMD, True, marks=pytest.mark.requires_orca
+        ),
+        pytest.param(
+            QmProg.TM, TmSolvMod.DCOSMORS, True, marks=pytest.mark.requires_turbomole
+        ),
         *[
             pytest.param(QmProg.ORCA, sm, False, marks=pytest.mark.requires_orca)
             for sm in SOLV_MODS[QmProg.ORCA.value]
@@ -41,13 +44,16 @@ def test_screening_integration(
     parallel_config: ParallelConfig,
     prog: QmProg,
     solvation_model: TmSolvMod | OrcaSolvMod | None,
-    gas_phase: bool,
+    evaluate_rrho: bool,
 ):
-    config.general.gas_phase = gas_phase
+    config.general.evaluate_rrho = evaluate_rrho
     config.screening.prog = prog
-    if not gas_phase and solvation_model is not None:
+    if solvation_model is not None:
+        config.general.gas_phase = False
         config.screening.sm = solvation_model
         config.general.solvent = "h2o"
+    else:
+        config.general.gas_phase = True
     config = PartsConfig.model_validate(config, context={"check": "screening"})
     timing = screening(ensemble_from_xyz, config, parallel_config, cut=True)
     assert timing is not None
