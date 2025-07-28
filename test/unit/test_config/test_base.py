9c9ee77a4afa26ff3c2de2fea2b114fe0ba1a7e2
lmseidler <seidler118@gmail.com>
2025-07-28 12:25:56 +0200

    test enum print

--- test/unit/test_config/test_base.py
+++ test/unit/test_config/test_base.py
@@ -1,5 +1,7 @@
 """Tests for BasePartConfig"""
 
+import enum
+
 from censo.config.parts.base import BasePartConfig
 from censo.params import PLENGTH
 
@@ -7,11 +9,18 @@ from censo.params import PLENGTH
 def test_base_part_config_str():
     """Test the string representation of BasePartConfig"""
 
+    class TestEnum(enum.Enum):
+        """Test enum"""
+
+        A = "a"
+        B = "b"
+
     class TestBaseConfig(BasePartConfig):
         """Test implementation of BasePartConfig"""
 
         test_value: str = "test"
         test_number: int = 42
+        test_enum: TestEnum = TestEnum.A
 
     config = TestBaseConfig()
     result = str(config)
@@ -24,6 +33,7 @@ def test_base_part_config_str():
     assert "test" in result
     assert "test_number" in result
     assert "42" in result
+    assert "TestEnum" not in result
 
     # Check alignment
     lines = result.split("\n")
