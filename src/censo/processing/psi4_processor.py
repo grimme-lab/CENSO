4363a899de1b01e8be25aa8f677bc688a986997f
Michael Wang <michael_wzq@icloud.com>
2025-11-28 19:40:08 +1100

    add psi4_processor.py

---
+++ src/censo/processing/psi4_processor.py
@@ -0,0 +1,12 @@
+"""
+Contains Psi4Proc class for calculating psi4 related properties of conformers.
+"""
+
+import typing
+
+from .qm_processor import QmProc
+
+
+@typing.final
+class Psi4Proc(QmProc):
+    pass
