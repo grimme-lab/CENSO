47bad3cfae371d81de682e2dc00c7dab3c202b67
Michael Wang <michael_wzq@icloud.com>
2025-12-01 07:16:16 +1100

    implement opt for psi4

--- src/censo/processing/psi4_processor.py
+++ src/censo/processing/psi4_processor.py
@@ -7,12 +7,14 @@ import pathlib
 import typing
 
 from ..config.job_config import (
+    OptJobConfig,
     SPJobConfig,
 )
 from .job import (
     JobContext,
 )
 from .results import (
+    OptResult,
     SPResult,
     MetaData,
 )
@@ -39,10 +41,10 @@ class Psi4Proc(QmProc):
         xyzfile: str | None = None,
     ) -> list[str]:
         """
-        Prepares an OrderedDict to be fed into the parser in order to write an input file for jobtype 'jobtype'
+        Prepares an list of str to be written an input file for jobtype 'jobtype'
         (e.g. sp).
 
-        Can load up a template file from user assets folder.
+        TODO: use a template file from user assets folder.
 
         :param job: JobContext object containing the job information
         :type job: JobContext
@@ -66,36 +68,30 @@ class Psi4Proc(QmProc):
         if template:
             logger.warn("template not implemented for psi4")
 
-        inp[:0] = self.__prep_main(jobtype, config, no_solv)
+        # basis set
+        inp.append(f"set basis {config.basis}")
+        inp.append('')
 
-        # prepare all options that are supposed to be placed before the
-        # geometry definition
-        inp[1:1] = self.__prep_pregeom(jobtype, config, job.omp)
+        # molecule
+        inp.append("molecule {")
+        for line in job.conf.toxyz()[2:]:
+            inp.append(f"\t{line.strip()}")
+        inp.append("}")
+        inp.append('')
 
-        # prepare the geometry
-        if template:
-            geom_line = next(inp.index(line) for line in inp if "{geom}" in line)
-            inp.pop(geom_line)
-            inp[geom_line:geom_line] = self.__prep_geom(
-                job.conf,
-                xyzfile,
-                job.charge,
-                job.unpaired,
-            )
-        else:
-            inp.extend(
-                self.__prep_geom(
-                    job.conf,
-                    xyzfile,
-                    job.charge,
-                    job.unpaired,
-                )
-            )
+        # functional
+        match jobtype:
+            case "sp":
+                inp.append(f'print("%f" % energy("{FUNCTIONALS[config.func][Prog.PSI4.value]}"))')
+            case "opt":
+                inp.append(f'print("%f" % optimize("{FUNCTIONALS[config.func][Prog.PSI4.value]}"))')
+            case unknown:
+                logger.warn(f"{unknown} is not implemented for psi4")
+        inp.append('')
 
-        inp.extend(self.__prep_postgeom(config, jobtype))
+        logger.warn("Grid is not implemnented for psi4")
 
-        # Finally append newlines where needed
-        return [line + "\n" if not line.endswith("\n") else line for line in inp]
+        return inp
 
     @typing.final
     def __sp(
@@ -148,24 +144,7 @@ class Psi4Proc(QmProc):
 
         # prepare input
         if prep:
-            inp = []
-
-            # basis set
-            inp.append(f"set basis {config.basis}")
-            inp.append('')
-
-            # molecule
-            inp.append("molecule {")
-            for line in job.conf.toxyz()[2:]:
-                inp.append(f"\t{line.strip()}")
-            inp.append("}")
-            inp.append('')
-
-            # functional
-            inp.append(f'print("%f" % energy("{FUNCTIONALS[config.func][Prog.PSI4.value]}"))')
-            inp.append('')
-
-            logger.warn("Grid is not implemnented for psi4")
+            inp = self.__prep(job, config, "sp", solvation)
 
             # write input into file "{filename}.inp" in a subdir
             # created for the conformer
@@ -206,9 +185,64 @@ class Psi4Proc(QmProc):
         :return: Tuple of (SP result, metadata).
         """
 
-        logger.warn("psi4 is not fully implemented")
+        logger.warn("sp is not fully implemented for psi4")
 
         return self.__sp(*args, **kwargs)
+
+    @typing.override
+    def opt(
+        self,
+        job: JobContext,
+        config: OptJobConfig,
+    ) -> tuple[OptResult, MetaData]:
+        """
+        Geometry optimization using psi4 optimizer.
+        Note that solvation in handled here always implicitly.
+
+        :param job: JobContext object containing the job information, metadata is stored in job.meta
+        :param config: Optimization configuration
+        :return: Tuple of (OptResult, MetaData)
+        """
+
+        logger.warn("opt is not fully implemented for psi4")
+
+        # prepare result
+        result = OptResult()
+        meta = MetaData(job.conf.name)
+
+        jobdir = self._setup(job, "opt")
+        filename = "opt"
+
+        # set orca input/output paths
+        inputpath = os.path.join(jobdir, f"{filename}.inp")
+        outputpath = os.path.join(jobdir, f"{filename}.out")
+
+        # prepare input dict
+        inp = self.__prep(job, config, "opt", no_solv=config.gas_phase)
+
+        # write input into file "{filename}.inp" in a subdir
+        # created for the conformer
+        with open(inputpath, "w") as f:
+            f.write('\n'.join(inp))
+
+        # read output
+        with open(outputpath) as out:
+            lines = out.readlines()
+
+        # Get final energy
+        try:
+            result.energy = next(
+                (
+                    float(line.split()[3])
+                    for line in lines
+                    if "Total Energy = " in line
+                ),
+            )
+        except StopIteration:
+            meta.success = False
+            meta.error = "Could not parse final energy"
+
+        return result, meta
 
 
 Factory.register_builder(Prog.PSI4, Psi4Proc)
