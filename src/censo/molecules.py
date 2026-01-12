331f86fd50ad0c6997d71917a2359f63d7bc8cb8
lmseidler <seidler118@gmail.com>
2026-01-12 12:54:33 +0100

    reformat docstrings

--- src/censo/molecules.py
+++ src/censo/molecules.py
@@ -10,6 +10,13 @@ from .params import BOHR2ANG, QmProg
 class Contributions:
     """
     Energy contributions for a molecule.
+
+    :ivar energy: Electronic energy contribution in Hartree.
+    :vartype energy: float
+    :ivar gsolv: Solvation free energy contribution in Hartree.
+    :vartype gsolv: float
+    :ivar grrho: Rigid-rotor-harmonic-oscillator (RRHO) free energy contribution in Hartree.
+    :vartype grrho: float
     """
 
     energy: float = 0.0
@@ -20,6 +27,11 @@ class Contributions:
 class Atom(BaseModel):
     """
     Represents an atom with element and coordinates.
+
+    :ivar element: Chemical element symbol (e.g., 'C', 'H', 'O').
+    :vartype element: str
+    :ivar xyz: Cartesian coordinates (x, y, z) in Angstrom.
+    :vartype xyz: tuple[float, float, float]
     """
 
     element: str
@@ -28,18 +40,30 @@ class Atom(BaseModel):
 
 class GeometryData:
     """
-    Geometry contains geometry information as well as identifier to match it to a MoleculeData object
-    in order to keep the object small, since it has to be pickled for multiprocessing
+    Container for molecular geometry information.
+
+    Stores atomic positions and provides conversion methods between different
+    coordinate formats (xyz, coord). Designed to be lightweight for efficient
+    pickling in multiprocessing contexts.
+
+    :ivar name: Identifier name of the geometry, used to match with MoleculeData objects.
+    :vartype name: str
+    :ivar xyz: List of Atom objects containing element symbols and Cartesian coordinates.
+    :vartype xyz: list[Atom]
+    :ivar nat: Number of atoms in the geometry.
+    :vartype nat: int
     """
 
     def __init__(self, name: str, atoms: list[Atom]):
         """
-        Takes an identifier and the geometry lines from the xyz-file as input.
+        Initialize a GeometryData object with atoms.
 
-        :param name: Name of the geometry.
+        :param name: Name identifier for the geometry.
         :type name: str
-        :param xyz: List of xyz lines.
-        :type xyz: list[str]
+        :param atoms: List of Atom objects. Also accepts list[str] for backwards
+            compatibility (deprecated).
+        :type atoms: list[Atom]
+        :raises ValueError: If xyz line format is invalid when parsing from strings.
         """
         # name of the linked MoleculeData
         self.name: str = name
@@ -68,6 +92,17 @@ class GeometryData:
 
     @classmethod
     def from_xyz(cls, name: str, xyz: list[str]):
+        """
+        Create a GeometryData object from xyz-file format lines.
+
+        :param name: Name identifier for the geometry.
+        :type name: str
+        :param xyz: List of strings in xyz format (element x y z).
+        :type xyz: list[str]
+        :return: New GeometryData instance.
+        :rtype: GeometryData
+        :raises ValueError: If line format is invalid (not 4 space-separated values).
+        """
         atoms = []
         # set up xyz dict from the input lines
         for line in xyz:
@@ -86,6 +121,18 @@ class GeometryData:
         atomic_numbers: list[int],
         coords: list[tuple[float, float, float]],
     ):
+        """
+        Create a GeometryData object from atomic numbers and coordinates.
+
+        :param name: Name identifier for the geometry.
+        :type name: str
+        :param atomic_numbers: List of atomic numbers (e.g., [6, 1, 1] for CH2).
+        :type atomic_numbers: list[int]
+        :param coords: List of (x, y, z) coordinate tuples in Angstrom.
+        :type coords: list[tuple[float, float, float]]
+        :return: New GeometryData instance.
+        :rtype: GeometryData
+        """
         from .params import PSE
 
         atoms = [Atom(element=PSE[i], xyz=c) for i, c in zip(atomic_numbers, coords)]
@@ -93,9 +140,9 @@ class GeometryData:
 
     def toorca(self) -> list[str]:
         """
-        Method to convert the internal cartesian coordinates to a data format usable by the OrcaParser.
+        Convert internal cartesian coordinates to ORCA format.
 
-        :return: List of coordinate strings.
+        :return: List of coordinate strings in ORCA format (element x y z).
         :rtype: list[str]
         """
         coord = []
@@ -106,9 +153,11 @@ class GeometryData:
 
     def tocoord(self) -> list[str]:
         """
-        Method to convert the internal cartesian coordinates (self.xyz) to coord file format (for tm or xtb).
+        Convert internal cartesian coordinates to TURBOMOLE coord file format.
 
-        :return: List of coord lines.
+        Coordinates are converted from Angstrom to Bohr for TURBOMOLE/XTB compatibility.
+
+        :return: List of strings in coord file format, including $coord and $end markers.
         :rtype: list[str]
         """
         coord = ["$coord\n"]
@@ -125,7 +174,12 @@ class GeometryData:
         return coord
 
     def fromcoord(self, *args):
-        """Wrapper for deprecated code."""
+        """
+        Deprecated wrapper for update_from_coord_file.
+
+        .. deprecated::
+            Use update_from_coord_file instead.
+        """
         warnings.warn(
             "fromcoord is deprecated, use update_from_coord_file instead",
             DeprecationWarning,
@@ -134,12 +188,14 @@ class GeometryData:
 
     def update_from_coord_file(self, path: str) -> None:
         """
-        Method to convert the content of a coord file to cartesian coordinates for the 'xyz' attribute.
+        Update geometry from a TURBOMOLE coord file.
+
+        Reads a coord file and updates the xyz attribute with parsed coordinates.
+        Coordinates are automatically converted from Bohr to Angstrom.
 
         :param path: Path to the coord file.
         :type path: str
-        :return: None
-        :rtype: None
+        :raises ValueError: If coord line format is invalid.
         """
         with open(path) as file:
             lines = file.readlines()
@@ -157,7 +213,12 @@ class GeometryData:
                 break
 
     def fromxyz(self, *args):
-        """Wrapper for deprecated code."""
+        """
+        Deprecated wrapper for update_from_xyz_file.
+
+        .. deprecated::
+            Use update_from_xyz_file instead.
+        """
         warnings.warn(
             "fromxyz is deprecated, use update_from_xyz_file instead",
             DeprecationWarning,
@@ -166,12 +227,13 @@ class GeometryData:
 
     def update_from_xyz_file(self, path: str) -> None:
         """
-        Method to convert the content of an xyz file to cartesian coordinates for the 'xyz' attribute.
+        Update geometry from an xyz file.
+
+        Reads an xyz file and updates the xyz attribute with parsed coordinates.
+        Skips the first two header lines (atom count and comment).
 
         :param path: Path to the xyz file.
         :type path: str
-        :return: None
-        :rtype: None
         """
         with open(path) as file:
             lines = file.readlines()
@@ -186,9 +248,12 @@ class GeometryData:
 
     def toxyz(self) -> list[str]:
         """
-        Method to convert self.xyz to xyz-file format.
+        Convert geometry to xyz-file format.
 
-        :return: List of xyz lines.
+        :return: List of strings in xyz format:
+            - Line 1: number of atoms
+            - Line 2: molecule name
+            - Remaining lines: element x y z (coordinates in Angstrom)
         :rtype: list[str]
         """
         lines = [
@@ -205,22 +270,42 @@ class GeometryData:
 
 class MoleculeData:
     """
-    The confomers' MoleculeData are set up in censo.ensemble.EnsembleData.setup_conformers
+    Container for molecular data including geometry and energy contributions.
+
+    Stores conformer information including geometry, charge, spin state, and
+    energy contributions. Conformers are typically set up in
+    censo.ensemble.EnsembleData.setup_conformers.
+
+    :ivar name: Identifier name of the molecule/conformer.
+    :vartype name: str
+    :ivar geom: GeometryData object containing atomic coordinates.
+    :vartype geom: GeometryData
+    :ivar degen: Degeneracy factor of the conformer (default: 1).
+    :vartype degen: int
+    :ivar charge: Molecular charge.
+    :vartype charge: int
+    :ivar unpaired: Number of unpaired electrons.
+    :vartype unpaired: int
+    :ivar xtb_energy: Initial (biased) xTB energy from CREST or similar (optional).
+    :vartype xtb_energy: float | None
+    :ivar mo_paths: Dictionary storing paths to molecular orbital files by QM program.
+    :vartype mo_paths: dict[str, list[str | tuple[str, str]]]
     """
 
     def __init__(
         self, name: str, geom: GeometryData, charge: int = 0, unpaired: int = 0
     ):
         """
-        Takes geometry lines from the xyz-file as input to pass it to the GeometryData constructor.
+        Initialize a MoleculeData object.
 
-        :param name: Name of the molecule.
+        :param name: Name identifier for the molecule.
         :type name: str
-        :param xyz: List of xyz lines.
-        :type xyz: list[str]
-        :param charge: Charge of the molecule.
+        :param geom: GeometryData object. Also accepts list[str] for backwards
+            compatibility (deprecated).
+        :type geom: GeometryData
+        :param charge: Molecular charge (default: 0).
         :type charge: int
-        :param unpaired: Number of unpaired electrons.
+        :param unpaired: Number of unpaired electrons (default: 0).
         :type unpaired: int
         """
 
@@ -269,8 +354,20 @@ class MoleculeData:
         unpaired: int = 0,
     ):
         """
-        Constructor to create a MoleculeData object from a list of atomic numbers and
-        cartesian coordinates in Angstrom.
+        Create a MoleculeData object from atomic numbers and coordinates.
+
+        :param name: Name identifier for the molecule.
+        :type name: str
+        :param atomic_numbers: List of atomic numbers (e.g., [6, 1, 1] for CH2).
+        :type atomic_numbers: list[int]
+        :param coords: List of (x, y, z) coordinate tuples in Angstrom.
+        :type coords: list[tuple[float, float, float]]
+        :param charge: Molecular charge (default: 0).
+        :type charge: int
+        :param unpaired: Number of unpaired electrons (default: 0).
+        :type unpaired: int
+        :return: New MoleculeData instance.
+        :rtype: MoleculeData
         """
         geom = GeometryData.from_mol(name, atomic_numbers, coords)
         return cls(name, geom, charge=charge, unpaired=unpaired)
@@ -278,7 +375,18 @@ class MoleculeData:
     @classmethod
     def from_xyz(cls, name: str, xyz: list[str], charge: int = 0, unpaired: int = 0):
         """
-        Constructor to create a MoleculeData object from a list of xyz-file lines.
+        Create a MoleculeData object from xyz-file format lines.
+
+        :param name: Name identifier for the molecule.
+        :type name: str
+        :param xyz: List of strings in xyz format (element x y z).
+        :type xyz: list[str]
+        :param charge: Molecular charge (default: 0).
+        :type charge: int
+        :param unpaired: Number of unpaired electrons (default: 0).
+        :type unpaired: int
+        :return: New MoleculeData instance.
+        :rtype: MoleculeData
         """
         geom = GeometryData.from_xyz(name, xyz)
         return cls(name, geom, charge=charge, unpaired=unpaired)
@@ -286,9 +394,9 @@ class MoleculeData:
     @property
     def energy(self) -> float:
         """
-        Current energy.
+        Electronic energy of the molecule.
 
-        :return: Energy value.
+        :return: Electronic energy in Hartree.
         :rtype: float
         """
         return self.__energy
@@ -296,9 +404,9 @@ class MoleculeData:
     @property
     def gsolv(self) -> float:
         """
-        Current gsolv.
+        Solvation free energy contribution.
 
-        :return: Gsolv value.
+        :return: Solvation free energy (Gsolv) in Hartree.
         :rtype: float
         """
         return self.__gsolv
@@ -306,9 +414,9 @@ class MoleculeData:
     @property
     def grrho(self) -> float:
         """
-        Current grrho.
+        Rigid-rotor-harmonic-oscillator (RRHO) free energy contribution.
 
-        :return: Grrho value.
+        :return: RRHO free energy (Grrho) in Hartree.
         :rtype: float
         """
         return self.__grrho
@@ -316,21 +424,19 @@ class MoleculeData:
     @property
     def gtot(self) -> float:
         """
-        Current energy+gsolv+grrho.
+        Total free energy (sum of all contributions).
 
-        :return: Total free energy.
+        :return: Total free energy (energy + gsolv + grrho) in Hartree.
         :rtype: float
         """
         return self.__energy + self.__gsolv + self.__grrho
 
     def update(self, contributions: Contributions):
         """
-        Update contributions.
+        Update energy contributions for this molecule.
 
-        :param contributions: Contributions to update with.
+        :param contributions: Contributions object containing energy, gsolv, and grrho values.
         :type contributions: Contributions
-        :return: None
-        :rtype: None
         """
         self.__energy = contributions.energy
         self.__gsolv = contributions.gsolv
